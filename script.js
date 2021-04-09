da2b3cb47e5bde3b496f4ead099b8e097ff8fc6b
Gustavo Gomes <gustsgomesbr@hotmail.com>
2021-04-08 22:30:51 -0400

    script added, games object created

---
+++ script.js
@@ -0,0 +1,72 @@
+const ourGames = [
+    {
+        name: "Apex Legends",
+        img: "",
+        category: ["Action", "Adventure"]
+    },
+    {
+        name: "Albion Online",
+        img: "",
+        category: ["Adventure", "Action", "MMO"]
+    },
+    {
+        name: "Brawlhalla",
+        img: "",
+        category: ["Adventure"]
+    },
+    {
+        name: "Counter-Strike: Global Offensive",
+        img: "",
+        category: ["Action"]
+    },
+    {
+        name: "Destiny 2",
+        img: "",
+        category: ["Action", "Adventure"]
+    },
+    {
+        name: "Dota 2",
+        img: "",
+        category: ["Action", "MMO"]
+    },
+    {
+        name: "DSC World",
+        img: "",
+        category: ["Simulation"]
+    },
+    {
+        name: "Team Fortress 2",
+        img: "",
+        category: ["Adventure", "Action"]
+    },{
+        name: "Trove",
+        img: "",
+        category: ["Adventure"]
+    },
+    {
+        name: "Paladins",
+        img: "",
+        category: ["Action", "Strategy"]
+    },
+    {
+        name: "Warframe",
+        img: "",
+        category: ["Adventure", "Action"]
+    },
+    {
+        name: "War Thunder",
+        img: "",
+        category: ["Adventure", "Action", "Simulation"]
+    },
+    {
+        name: "Word of Tanks Blitz",
+        img: "",
+        category: ["Action", "Simulation"]
+    },
+    {
+        name: "Word of Warships",
+        img: "",
+        category: ["Adventure", "Action", "Simulation"]
+    }
+
+]
